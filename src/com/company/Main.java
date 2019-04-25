c13b2bdde7a2e0be3f97a573671420f1eb93a0f0
ALSAYED Amal <alsayedama@GRNATL0403.bureau.si.interne>
2019-04-25 17:36:30 +0200

    my work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,96 +1,131 @@
-package com.company;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
-import java.util.*;
+public class main {
 
-public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Bienvenue au jeu Masrter Mind");
+        System.out.println("Il faut vous devinez le code. Le code est 4 balls colorés.");
+        System.out.println("Les couleurs sont : bleu, vert, rouge, orange,  jaune, marron, violet, noir.");
 
-    public static void main(String[] args) {
 
-        List<String> couleurs = new ArrayList<>(Arrays.asList("bleu", "vert", "rouge", "orange", "jaune", "marron", "violet", "noir"));
-        List<String> codeSecret = new ArrayList<>();
-        int nbEssai = 12;
 
-        // Creation du code secret
-        for (int i = 0; i < 4; i++) {
-            int auHasard = (int) (Math.random() * (couleurs.size()));
-            codeSecret.add(couleurs.get(auHasard));
-        }
 
-        System.out.println("Liste des couleurs = " + couleurs);
-        System.out.println("code secret = " + codeSecret);
 
-        while (nbEssai != 0) {
-            System.out.println("Il vous reste " + nbEssai + " essai(s)");
-            if (comparer(tentative(), codeSecret) == true) {
-                System.out.println("Gagne !");
-                break;
-            }
-            nbEssai -= 1;
-        }
-        if (nbEssai == 0) {
-            System.out.println("Perdu...");
-        }
+        ArrayList listeDeCode = new ArrayList();
+        ArrayList listeDeChoix = new ArrayList();
+
+        ArrayList couleurs = new ArrayList();
+        couleurs.add("bleu");
+        couleurs.add("vert");
+        couleurs.add("rouge");
+        couleurs.add("orange");
+        couleurs.add("jaune");
+        couleurs.add("marron");
+        couleurs.add("violet");
+        couleurs.add("noir");
+
+
+
+
+        listeDeCode = (ArrayList) proposeCode(couleurs);
+        System.out.println(listeDeCode);
+
+        listeDeChoix= (ArrayList) saisirChoix();
+        System.out.println(listeDeChoix);
+
+        System.out.println(verifierResultat(listeDeCode, listeDeChoix));
+
+
+
+
+
+
+
     }
 
-    // Methode permettant de comparer une proposition et le code secret
-    public static boolean comparer(List propositions, List codeSecret) {
-        int compteur = 0;
-        boolean resultat = false;
-        for (int i = 0; i < codeSecret.size(); i++) {
-            if (propositions.get(compteur).equals(codeSecret.get(compteur))) {
-                System.out.println(propositions.get(compteur) + " est bien place");
-            } else if (codeSecret.contains(propositions.get(compteur)) && !propositions.get(compteur).equals(codeSecret.get(compteur))) {
-                System.out.println(propositions.get(compteur) + " est mal place");
-            } else {
-                System.out.println(propositions.get(compteur) + " n'est pas dans le code");
+    // Les Methodes :
+
+    // Function proposer le code.
+    public static List proposeCode(List listeDeCouleurs)
+    {
+
+        int couleurhazar = 0;
+        ArrayList code = new ArrayList();
+        Random r = new Random();
+
+        for (int i = 0; i < 4; i++)
+        {
+            couleurhazar = (int) (Math.random() * listeDeCouleurs.size());
+            code.add(listeDeCouleurs.get(couleurhazar));
+        }
+        return code;
+    }
+
+    // Function saisir le choix.
+    public static List saisirChoix()
+    {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList choix = new ArrayList();
+        for (int i =0; i<4; i++)
+        {
+            System.out.println("Entrer votre choix : ");
+            String value = sc.nextLine();
+            choix.add(value);
+        }
+        return choix;
+    }
+
+    // Function vérifier le résultat.
+    public static List verifierResultat(List verifierDeCode, List verifierDeChoix)
+    {
+
+        ArrayList resultat = new ArrayList();
+        for (int i =0; i<4; i++)
+        {
+            if (verifierDeCode.get(i).equals(verifierDeChoix.get(i)))
+            {
+                resultat.add("reussi");
             }
-            compteur += 1;
-            if (codeSecret.equals(propositions)) {
-                resultat = true;
+            else
+            {
+                resultat.add("echoué");
             }
         }
+
         return resultat;
     }
 
-    // Methode permettant d'écrire une proposition et de vérifier si cette dernière est conforme
-//    public static List tentative() {
-//        List<String> couleurs = new ArrayList<>(Arrays.asList("bleu", "vert", "rouge", "orange", "jaune", "marron", "violet", "noir"));
-//        List<String> propositions = new ArrayList<>();
-//        Scanner proposition = new Scanner(System.in);
-//        String couleurChoisie = "";
-//        for (int i = 0; i < 4; i++) {
-//            System.out.println("choisissez une couleur");
-//            do {
-//                couleurChoisie = proposition.nextLine();
-//                if (!couleurs.contains(couleurChoisie)) {
-//                    System.out.println("veuillez ressaisir un mot parmis les couleurs proposées sans espace ni majuscule");
-//                }
-//            } while (!couleurs.contains(couleurChoisie));
-//            propositions.add(couleurChoisie);
-//        }
-//        return propositions;
-//    }
 
-    // Methode permettant d'écrire une proposition (de 4 couleurs d'un coup) et de vérifier si cette dernière est conforme
-    public static List tentative() {
-        List<String> couleurs = new ArrayList<>(Arrays.asList("bleu", "vert", "rouge", "orange", "jaune", "marron", "violet", "noir"));
-        List<String> propositions = new ArrayList<>();
-        Scanner proposition = new Scanner(System.in);
-        String couleurChoisie = "";
-        while (propositions.size() != 4) {
-            propositions.clear();
-            System.out.println("Choisissez vos 4 couleurs separé par des virgules (mais sans espace)");
-            couleurChoisie = proposition.nextLine();
-            couleurChoisie += ",";
-            for (int i = 0; i < couleurChoisie.length(); i++) {
-                int sub = couleurChoisie.indexOf(",");
-                String couleur = couleurChoisie.substring(0, sub);
-                couleurChoisie = couleurChoisie.substring(sub + 1);
-                if (!couleurs.contains(couleur)) {
-                    System.out.println("Erreur : saisie erronné");
-                } else propositions.add(couleur);
-            }
-        }
-        return propositions;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
